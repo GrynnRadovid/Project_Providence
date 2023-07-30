09f3e98773e6c8c8d31fe08a7e68fb16dc1e123d
GrynnRadovid <126412429+GrynnRadovid@users.noreply.github.com>
2023-07-30 22:44:42 +0300

    Added Secure authentication logic for registration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,35 +1,44 @@
-// eslint-disable-next-line no-unused-vars
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
 import Contact from "./routes/Contact";
 import Registration from "./routes/Registration";
-import ErrorPage from "./error-page";
+import Login from "./routes/Login"; 
+import ErrorPage from "./routes/error-page";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
-    
-  },
-  {
-    path: "/registration",
-    element: <Registration />,
-  },
-]);
+const App = () => {
+  return (
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/registration">Registration</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link> {/* Add the login link */}
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/login" element={<Login />} /> {/* Add the Login route */}
+        <Route path="*" element={<ErrorPage />} /> {/* Handle 404 error */}
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
