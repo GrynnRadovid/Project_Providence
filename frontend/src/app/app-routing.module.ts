963d7b6ea63e105cfaca9a0f25349d158307e310
GrynnRadovid <126412429+GrynnRadovid@users.noreply.github.com>
2023-08-01 21:32:38 +0300

    Added routing to main-page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistrationComponent } from './routes/registration/registration.component';
 import { LoginComponent } from './routes/login/login.component';
-import { HomeComponent } from './routes/home/home.component';
+import { MainComponent } from './routes/main/main.component';
 import { AboutComponent } from './routes/about/about.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: MainComponent },
   { path: 'registration', component: RegistrationComponent },
   { path: 'login', component: LoginComponent },
   { path: 'about', component: AboutComponent }
