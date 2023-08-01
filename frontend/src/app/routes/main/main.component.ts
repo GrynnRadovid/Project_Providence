43f90844f86c26da98fef0e1a1e862414e44079d
GrynnRadovid <126412429+GrynnRadovid@users.noreply.github.com>
2023-08-01 21:31:54 +0300

    Added main-page component

---
+++ frontend/src/app/routes/main/main.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css'] // or .scss if you used that
+})
+export class MainComponent implements OnInit, OnDestroy {
+  isLoggedIn: boolean = false;
+  username: string = '';
+  private authSubscription?: Subscription;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    // Initialize states based on AuthService
+    this.isLoggedIn = this.authService._isAuthenticated; // Assuming isAuthenticated() checks the token or user session
+    this.username = this.authService.getUsername();  // Assuming getUsername() returns the username
+
+    // Optionally, if your AuthService has an Observable for auth changes:
+    this.authSubscription = this.authService.authChange.subscribe(isAuthenticated => {
+      this.isLoggedIn = isAuthenticated;
+      if (isAuthenticated) {
+        this.username = this.authService.getUsername();
+      } else {
+        this.username = '';
+      }
+    });
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.isLoggedIn = false;
+  }
+
+  ngOnDestroy(): void {
+    // Clean up the subscription when the component gets destroyed
+    this.authSubscription?.unsubscribe();
+  }
+}
